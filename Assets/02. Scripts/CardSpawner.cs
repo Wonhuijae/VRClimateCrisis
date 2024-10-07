b61a58d247e35ec1ede767452b32d552aba1ae8a
Wonhuijae <whj46262@gmail.com>
2024-10-07 22:52:28 +0900

    2024.10.17 카드 스포너 구현 중

--- Assets/02. Scripts/CardSpawner.cs
+++ Assets/02. Scripts/CardSpawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public enum CardType
 {
     Governmental,
@@ -13,5 +14,21 @@ public class CardSpawner : MonoBehaviour
 {
     public GameObject cardPrefab;
 
+    public PerCard[] perCards;
+    public CorCard[] corCards;
+    public GovCard[] govCards;
 
+    public Transform[] spawnPos;
+
+    private void Start()
+    {
+           
+    }
+
+    public void DrawCards()
+    {
+        Instantiate(cardPrefab, spawnPos[0].position, Quaternion.identity);
+        Instantiate(cardPrefab, spawnPos[1].position, Quaternion.identity);
+        Instantiate(cardPrefab, spawnPos[2].position, Quaternion.identity);
+    }
 }
--- Assets/02. Scripts/Scriptable Object Class/CardData.cs
+++ Assets/02. Scripts/Scriptable Object Class/CardData.cs
@@ -8,7 +8,39 @@ public class CardData : ScriptableObject
 {
     [SerializeField] public string cardName;
     [SerializeField] public string cardContent;
-    [SerializeField] public CardType cardType;
     [SerializeField] public Color cardColor;
     [SerializeField] public float carbonReduce;
+}
+
+[CreateAssetMenu(menuName = "Scriptable/PerCard", fileName = "PerCard")]
+public class PerCard : CardData
+{
+    [SerializeField] public CardType cardType = CardType.Personal;
+
+    public PerCard()
+    {
+        cardColor = Color.white;
+    }
+}
+
+[CreateAssetMenu(menuName = "Scriptable/CorCard", fileName = "CorCard")]
+public class CorCard : CardData
+{
+    [SerializeField] public CardType cardType = CardType.Corporate;
+
+    public CorCard()
+    {
+        cardColor = new Color(1, (float)245 / 255, 0, 1);
+    }
+}
+
+[CreateAssetMenu(menuName = "Scriptable/GovCard", fileName = "GovCard")]
+public class GovCard : CardData
+{
+    [SerializeField] public CardType cardType = CardType.Governmental;
+
+    public GovCard()
+    {
+        cardColor = new Color(1, (float)149 / 255, 0, 1);
+    }
 }
